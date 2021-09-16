60ec5cd88f5d289840f7f394f7b051388e943e0a
Chiemeke <83980905+Chiemeke@users.noreply.github.com>
2021-09-16 10:33:15 +0100

    git ignore node modules

--- api/configuration/db.js
+++ api/configuration/db.js
@@ -1,6 +1,6 @@
 const  Sequelize  = require('sequelize');
 
-const sequelize = new Sequelize("node_shop2", "chiemeke", "Password123.", {
+const sequelize = new Sequelize("node_shop2", process.env.DB_USER, process.env.DB_PASSWORD, {
     host: "localhost",
     dialect: "mysql"
   });
